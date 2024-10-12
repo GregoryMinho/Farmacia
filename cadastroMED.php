eff6d5213a48513ca85651e507ec284b7d45f7fb
Gregory Minho <gregoryminho@gmail.com>
2024-10-12 18:35:53 -0300

    2

--- cadastroMED.php
+++ cadastroMED.php
@@ -7,9 +7,9 @@
 </head>
 <body>
     <h1>Cadastro de Medicamentos</h1>
-    <form>
-        <label for="nome-medicamento">Nome do medicamento:</label><br>
-        <input type="text" id="nomeMed" name="nomeMed" required><br><br>
+    <form action="cadastroMED.php" method="$_GET">
+        <label for="nome">Nome do medicamento:</label><br>
+        <input type="text" id="nome" name="nome" required><br><br>
 
         <label for="preco">Preço unitário (R$):</label><br>
         <input type="number" id="preco" name="preco" required><br><br>
@@ -18,15 +18,10 @@
         <input type="number" id="quantidade" name="quantidade" required><br><br>
 
         <label for="categoria">Categoria:</label><br>
-        <select id="categoria" name="categoria" required>
-            <option value="">Selecione uma categoria</option>
-            <option value="Analgesico">Analgésico</option>
-            <option value="Antibiotico">Antibiótico</option>
-            <option value="Anti-inflamatorio">Anti-inflamatório</option>
-        </select><br><br>
+        <input type="text" id="categoria" name="categoria" required><br><br>
 
-        <label for="data-validade">Data de validade:</label><br>
-        <input type="date" id="dataValidade" name="dataValidade" required><br><br>
+        <label for="validade">Data de validade:</label><br>
+        <input type="date" id="validade" name="validade" required><br><br>
 
         <input type="submit" value="Cadastrar">
     </form>
